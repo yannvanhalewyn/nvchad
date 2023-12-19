ac18e2d54a8ce8c8902e1508bc95b029cf36cfa0
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2023-12-20 00:08:10 +0100

    Add basic mappings

--- mappings.lua
+++ mappings.lua
@@ -1,15 +1,26 @@
----@type MappingsTable
 local M = {}
 
-M.general = {
+-- -- In order to disable a default keymap, use
+M.disabled = {
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-  },
-  v = {
-    [">"] = { ">gv", "indent"},
+      ["<leader>b"] = "",
+      -- ["<C-a>"] = ""
+  }
+}
+
+M.abc = {
+  n = {
+    ["[b"] = {"<cmd>bprev<CR>", "Previous Buffer"},
+    ["]b"] = {"<cmd>bnext<CR>", "Next Buffer"},
+    ["<leader>n"] = {"<cmd>NvimTreeToggle<CR>", "Toggle Nvim Tree"},
+    ["<leader> "] = { "<cmd>Telescope find_files<CR>", "Find files" },
+    ["<leader>bb"] = { "<cmd>Telescope buffers<CR>", "Find buffers" },
+    ["<leader>bd"] = { "<cmd>bdelete<CR>", "Delete current buffer" },
+    ["<C-n>"] = {"<cmd>Telescope<CR>", "Telescope"},
+    ["<C-s>"] = {":Telescope Files<CR>", "Telescope Files"}
   },
 }
 
--- more keybinds!
+-- vim.g.maplocalleader = ","
 
 return M
