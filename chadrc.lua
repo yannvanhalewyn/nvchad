8ad36ead88fb208424f54e47c11defc3e502d1f2
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-01-08 20:13:22 +0100

    Enable netrw with NVChad

--- chadrc.lua
+++ chadrc.lua
@@ -17,4 +17,15 @@ M.plugins = "custom.plugins"
 -- check core.mappings for table structure
 M.mappings = require "custom.mappings"
 
+-- Enable netrw as ChadNV disables it by default
+M.lazy_nvim = {
+  performance = {
+    rtp = {
+      disabled_plugins = vim.tbl_filter(function(name)
+        return string.sub(name, 1, 5) ~= "netrw"
+      end, require("plugins.configs.lazy_nvim").performance.rtp.disabled_plugins),
+    },
+  },
+}
+
 return M
