e92acf71820d19c7c78367e6cc8e8d663d0d65c6
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-01-08 07:18:27 +0100

    Fixup

--- plugins.lua
+++ plugins.lua
@@ -64,8 +64,8 @@ local plugins = {
   {
     "theprimeagen/harpoon",
     keys = {
-      { "<leader>a", function() require("harpoon.mark").add_file() end, desc = "Harpoon Add File" },
-      { "<leader>H", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon Quick Menu" },
+      { "<leader>ha", function() require("harpoon.mark").add_file() end, desc = "Harpoon Add File" },
+      { "<leader>hh", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon Quick Menu" },
       { "<leader>1", function() require("harpoon.ui").nav_file(1) end, desc = "Harpoon Browse File (1)" },
       { "<leader>2", function() require("harpoon.ui").nav_file(2) end, desc = "Harpoon Browse File (2)" },
       { "<leader>3", function() require("harpoon.ui").nav_file(3) end, desc = "Harpoon Browse File (3)" },
