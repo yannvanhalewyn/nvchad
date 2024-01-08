868d9f346fe32d06064ed3e684f875e6eb522233
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-01-08 07:08:21 +0100

    Setup navigation keys in command-line-mode

--- mappings.lua
+++ mappings.lua
@@ -11,6 +11,15 @@ M.disabled = {
   }
 }
 
+vim.keymap.set("c", "<C-a>", "<Home>")
+vim.keymap.set("c", "<C-e>", "<End>")
+vim.keymap.set("c", "<C-p>", "<Up>")
+vim.keymap.set("c", "<C-n>", "<Down>")
+vim.keymap.set("c", "<C-b>", "<Left>")
+vim.keymap.set("c", "<C-f>", "<Right>")
+vim.keymap.set("c", "<M-b>", "<S-Left>")
+vim.keymap.set("c", "<M-f>", "<S-Right>")
+
 M.abc = {
   n = {
     ["[b"] = {"<cmd>bprev<CR>", "Prev Buffer"},
