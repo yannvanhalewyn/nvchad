e2ac7a2fe34e4f3b5b71f45f27e37030af00e0b7
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-01-08 05:52:33 +0100

    Cleanup harpoon config

--- mappings.lua
+++ mappings.lua
@@ -17,13 +17,13 @@ M.abc = {
     ["[w"] = {vim.cmd.tabprev, "Prev Tab"},
     ["]w"] = {vim.cmd.tabnext, "Next Tab"},
     ["<leader>wn"] = {vim.cmd.tabnew, "New Tab"},
+    ["<leader>wd"] = {vim.cmd.quit, "Quit window"},
     ["<leader>n"] = {"<cmd>NvimTreeToggle<CR>", "Toggle Nvim Tree"},
     ["<leader> "] = { "<cmd>Telescope find_files<CR>", "Find files" },
     ["<leader>bb"] = { "<cmd>Telescope buffers<CR>", "Find buffers" },
     ["<leader>bd"] = { "<cmd>bdelete<CR>", "Delete current buffer" },
     ["<leader>cr"] = { "<cmd> Telescope lsp_references <CR>", "[C]ode [R]references" },
     ["<leader>fr"] = { "<cmd> Telescope oldfiles <CR>", "[F]ind [R]ecent files" },
-    ["<C-s>"] = {":Telescope Files<CR>", "Telescope Files"}
   },
 }
 
--- plugins.lua
+++ plugins.lua
@@ -63,7 +63,6 @@ local plugins = {
   },
   {
     "theprimeagen/harpoon",
-    lazy = false, -- Needed for mapping
     keys = {
       { "<leader>a", function() require("harpoon.mark").add_file() end, desc = "Harpoon Add File" },
       { "<leader>H", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon Quick Menu" },
@@ -72,18 +71,6 @@ local plugins = {
       { "<leader>3", function() require("harpoon.ui").nav_file(3) end, desc = "Harpoon Browse File (3)" },
       { "<leader>4", function() require("harpoon.ui").nav_file(4) end, desc = "Harpoon Browse File (4)" },
     },
-    -- config = function()
-    --   local mark = require("harpoon.mark")
-    --   local ui = require("harpoon.ui")
-    --
-    --   vim.keymap.set("n", "<leader>a", mark.add_file)
-    --   vim.keymap.set("n", "<leader>H", ui.toggle_quick_menu)
-    --
-    --   vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-    --   vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-    --   vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-    --   vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
-    -- end
   },
   {
     "mbbill/undotree",
