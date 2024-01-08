7011a7df3d19747ac33085cbc64e9df9443e4ad9
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-01-08 07:07:31 +0100

    Replace null-ls with none-ls

--- plugins.lua
+++ plugins.lua
@@ -10,9 +10,9 @@ local plugins = {
     dependencies = {
       -- format & linting
       {
-        "jose-elias-alvarez/null-ls.nvim",
+        "nvimtools/none-ls.nvim",
         config = function()
-          require "custom.configs.null-ls"
+          require "custom.configs.none-ls"
         end,
       },
     },
