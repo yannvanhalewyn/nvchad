3392728c27b7b7d4da4e9c1ff8d922df15532a06
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-02-07 00:47:52 +0100

    Add some Clojure snippets and README todolist

--- snippets/clojure.lua
+++ snippets/clojure.lua
@@ -5,9 +5,10 @@ return {
   s("cl", fmt("(js/console.log {})", { i(1) })),
   s("pp", fmt("(clojure.pprint/pprint {})", { i(1) })),
   s("spy", fmt("(sc.api/spy {})", { i(1) })),
-  s("letsc", fmt("(sc.api/letsc {} {})", { i(1, "spypoint"), i(2, "body") })),
+  s("ds", fmt("(sc.api/defsc {})", { i(1, "spypoint") })),
+  s("ls", fmt("(sc.api/letsc {} {})", { i(1, "spypoint"), i(2, "body") })),
   s("req", fmt("(require '[{} :as {}])", { i(1, "example.ns"), i(2, "alias")})),
-  s("unalias", fmt("(ns-unalias *ns* {})", { i(1, "alias") })),
+  s("unalias", fmt("(ns-unalias *ns* '{})", { i(1, "alias") })),
   s("unmap", fmt("(ns-unmap *ns* {})", { i(1, "symbol") })),
   s(
     "write-edn",
