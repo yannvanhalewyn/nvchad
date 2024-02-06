39b38dbd8a46e98fb913456fe4bc2eb6f70d08ae
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-02-07 00:53:04 +0100

    Use full path in statusline and disable NVDash

--- chadrc.lua
+++ chadrc.lua
@@ -7,13 +7,45 @@ local highlights = require("custom.highlights")
 M.ui = {
   theme = "onedark",
   theme_toggle = { "onedark", "one_light" },
+  transparency = false,
 
   hl_override = highlights.override,
   hl_add = highlights.add,
 
+  statusline = {
+    overriden_modules = function(modules)
+      -- https://github.com/NvChad/ui/blob/v2.0/lua/nvchad/statusline/default.lua
+      local icon = " 󰈚 "
+      local path = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(vim.g.statusline_winid))
+      local name = (path == "" and "Empty ") or path:match("([^/\\]+)[/\\]*$")
+
+      if name ~= "Empty " then
+        local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+
+        if devicons_present then
+          local ft_icon = devicons.get_icon(name)
+          icon = (ft_icon ~= nil and " " .. ft_icon) or icon
+        end
+
+        name = " " .. name .. " "
+      end
+
+      -- % -> current file
+      -- :~ -> prevent tilde expansion
+      -- :. -> make it relative to current dir.
+      local relative_path = vim.fn.expand('%:~:.')
+      local path_info = "%#St_file_info#" .. icon .. relative_path .. "%#St_file_sep#" .. ""
+      modules[2] = path_info
+    end
+  },
+
   tabufline = {
     enabled = false
   },
+
+  -- nvdash = {
+  --   load_on_startup = true
+  -- }
 }
 
 M.plugins = "custom.plugins"
