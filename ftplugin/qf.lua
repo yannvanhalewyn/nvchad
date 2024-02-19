7a822e2dc069995e4eb5c21d5abc12c257f16459
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-02-20 00:53:34 +0100

    Remove unused bindings in quickfix ftplugin

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -1,3 +1,1 @@
 vim.keymap.set("n", "q", "<cmd>cclose<CR>", {buffer = true, desc = "Close Quickfix Window", })
--- vim.keymap.set("n", "<c-p>", "<cmd>cprev<CR>", {buffer = true, desc = "Prev Quickfix Result", })
--- vim.keymap.set("n", "<c-n>", "<cmd>cnext<CR>", {buffer = true, desc = "Next Quickfix Result", })
