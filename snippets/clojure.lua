8e236deac4272b6ab6974ac7a37a1bac7214e512
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2024-02-20 00:24:22 +0100

    Add Clojure snippets

--- snippets/clojure.lua
+++ snippets/clojure.lua
@@ -10,6 +10,8 @@ return {
   s("req", fmt("(require '[{} :as {}])", { i(1, "example.ns"), i(2, "alias")})),
   s("unalias", fmt("(ns-unalias *ns* '{})", { i(1, "alias") })),
   s("unmap", fmt("(ns-unmap *ns* {})", { i(1, "symbol") })),
+  s("wor", t("(set! *warn-on-reflection* true)")),
+  s("pnm", t("(set! *print-namespace-maps* false)")),
   s(
     "write-edn",
     fmt([[(binding [*print-namespace-maps* false]
@@ -24,21 +26,23 @@ return {
     "read-csv",
     fmt([[(with-open [rdr (io/reader (io/file {}))]
   (into [] (csv/read-csv rdr :separator \{})))
-    ]], {
-        i(1, "file"),
-        i(2, "separator"),
-      }
+    ]], { i(1, "file"), i(2, "separator"), }
     )
   ),
   s(
     "write-csv",
     fmt([[(with-open [writer (io/writer (io/file {}))]
   (csv/write-csv writer {}))
-  ]], {
-        i(1, "file"),
-        i(2, "csv-rows")
-      }
+]], { i(1, "file"), i(2, "csv-rows") }
     )
   ),
-  s("ra", t("(clojure.tools.namespace.repl/refresh-all)"), {})
+  s("ra", t("(clojure.tools.namespace.repl/refresh-all)"), {}),
+  s(
+    "middleware",
+    fmt([[(defn wrap-{} [handler {}]
+  (fn [req]
+    (handler req)))
+]], { i(1, "name"), i(2, "args") }
+    )
+  )
 }
